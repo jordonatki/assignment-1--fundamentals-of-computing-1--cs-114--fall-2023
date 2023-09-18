9106db74634ecb1a0c2e6410fad46ab6aae5d38e
jordonatki <143655877+jordonatki@users.noreply.github.com>
2023-09-18 16:25:10 -0400

    Part 1 Block Initials

--- Assignment1.java
+++ Assignment1.java
@@ -1,7 +1,18 @@
 // Import libraries here
-
+/* Goal: Print initials in large block letters,convert Fahrenheit to celsius   
 public class Assignment1 {
   public static void main(String[] args) {
-    // Add your code here
-  }
+    System.out.println();
+    System.out.println("JJ JJ JJ JJ       A A");
+    System.out.println("     JJ         AA   AA");
+    System.out.println("     JJ       AA       AA");
+    System.out.println("     JJ      AA AA A AA AA");
+    System.out.println("JJ   JJ    AA            AA");
+    System.out.println("JJ J JJ   AA              AA");
+
+
+    //Fahrenheit to Celsius
+  
+  }  
 }
+
